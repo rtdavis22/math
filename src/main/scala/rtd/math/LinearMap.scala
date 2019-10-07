aeec118a80ff355170f75b726a8394fafcaa8958
Rob Davis <rtd@uber.com>
2019-10-07 02:11:07 -0700

    Add product of linear maps, Hom, and End

--- src/main/scala/rtd/math/LinearMap.scala
+++ src/main/scala/rtd/math/LinearMap.scala
@@ -3,6 +3,15 @@ package rtd.math
 abstract class LinearMap[V, W, E](val v: VectorSpace[V, E], val w: VectorSpace[W, E]) {
   def apply(e: V): W
 
+  // The product of two linear maps is defined as the composition of the maps: (ST)(u) = S(Tu)
+  def *[Y](t: LinearMap[W, Y, E]): LinearMap[V, Y, E] = {
+    new LinearMap[V, Y, E](v, t.w) {
+      override def apply(e: V): Y = {
+        t.apply(LinearMap.this.apply(e))
+      }
+    }
+  }
+
   // T(v + w) = T(v) + T(w)
   def apply(e: v.AdditionExpression): w.AdditionExpression = {
     new w.AdditionExpression(
@@ -11,34 +20,44 @@ abstract class LinearMap[V, W, E](val v: VectorSpace[V, E], val w: VectorSpace[W
   }
 }
 
-// class AllLinearMaps extends VectorSpace... pg. 55
-
-// Probably merge this into the above
-object LinearMap {
-  // (S + T)(v) = Sv + Tv
-  def +[V,W,E](s: LinearMap[V,W,E], t: LinearMap[V,W,E]): LinearMap[V,W,E] = {
-    new LinearMap[V,W,E](s.v, s.w) {
-      def apply(e: V): W = {
+// The set of linear maps from V to W forms a vector space
+class Hom[V, W, E](val v: VectorSpace[V, E], val w: VectorSpace[W, E], field: Field[E]) extends VectorSpace[LinearMap[V, W, E], E](field) {
+  override def +(s: LinearMap[V, W, E], t: LinearMap[V, W, E]): LinearMap[V, W, E] = {
+    new LinearMap[V, W, E](v, w) {
+      override def apply(e: V): W = {
         w.+(s.apply(e), t.apply(e))
       }
     }
   }
 
-  // (cT)(v) = c(Tv)
-  def *[T,V,E](c: E, s: LinearMap[T,V,E]): LinearMap[T,V,E] = {
-    new LinearMap[T,V,E](s.v, s.w) {
-      def apply(e: T): V = {
+  override def *(c: E, s: LinearMap[V, W, E]): LinearMap[V, W, E] = {
+    new LinearMap[V, W, E](v, w) {
+      override def apply(e: V): W = {
         w.*(c, s.apply(e))
+      }
+    }
+  }
+
+  override def -(s: LinearMap[V, W, E]): LinearMap[V, W, E] = {
+    new LinearMap[V, W, E](v, w) {
+      override def apply(e: V): W = {
+        w.-(s.apply(e))
+      }
+    }
+  }
+
+  override def zero(): LinearMap[V, W, E] = {
+    new LinearMap[V, W, E](v, w) {
+      override def apply(e: V): W = {
+        w.zero()
       }
     }
   }
 }
 
-class ExampleLinearMap extends LinearMap(new RealVectorSpace[I3], new RealVectorSpace[I4]) {
-  override def apply(e: Coordinate[R, I3]): Coordinate[R, I4] = {
-    w.zero()
-  }
-}
+// The endomorphisms of a linear map V
+class End[V, E](v: VectorSpace[V, E], field: Field[E]) extends Hom[V, V, E](v, v, field)
+
 
 abstract class EndomorphicMap[T, E](v: VectorSpace[T, E]) extends LinearMap[T, T, E](v, v) {}
 
@@ -52,14 +71,12 @@ class ZeroMap[T, V, E](v1: VectorSpace[T, E], v2: VectorSpace[V, E]) extends Lin
 
 object LinearMapMain {
   def main(args: Array[String]): Unit = {
-    val m = new ExampleLinearMap
-    //val res = m.apply(new Coordinate[R, I3]())
-    //println(res)
-
     val vs = new RealVectorSpace[I3]()
     val m2 = new IdentityMap(new RealVectorSpace[I3]())
-    val m3 = LinearMap.+(m2, m2)
-    val res2 = m3.apply(vs.zero())
-    println(res2)
+
+    val hom = new Hom(vs, vs, new ℝ)
+    val zeroMap = hom.zero()
+    val res = zeroMap.apply(vs.zero())
+    println(res)
   }
 }
